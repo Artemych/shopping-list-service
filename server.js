b10d220ff58ef39fcc8cbd0090fc2e989a6dddbb
artem <temamochalov@yandex.ru>
2014-09-22 22:37:51 +0400

    add content type header

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express'),
 	commands = require('./commands'),
 	app = express();
 
+app.use(function(req, res, next){
+	res.header("Content-Type", "application/json");
+	next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.multipart());
+
 app.get('/product/list', commands.findAll);
 app.get('/product/:id', commands.findById);
+//app.post('/product', commands.addNew);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
